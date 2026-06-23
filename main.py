efafb392cc9fd3078a5d902928bb2cfa7eedabbd
anubhuti-sh <anubhutiv@sensehawk.com>
2026-06-23 12:12:59 +0530

    adds support for multiple connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import threading
 
 API_VERSIONS_KEY = 18
 
@@ -50,21 +51,57 @@ class MyKafka:
         conn.sendall(data)
         return "OK"
 
+    def _read_message(self, conn):
+        length_bytes = conn.recv(4)
+        if not length_bytes or len(length_bytes) < 4:
+            return None
+        message_size = struct.unpack(">i", length_bytes)[0]
+        data = length_bytes
+        while len(data) < 4 + message_size:
+            chunk = conn.recv(4 + message_size - len(data))
+            if not chunk:
+                return None
+            data += chunk
+        return data
+
+    def _handle_request(self, data, conn):
+        header = parse_request_header(data)
+        if header is None:
+            return False
+        if header["api_key"] == API_VERSIONS_KEY:
+            response = build_api_versions_response(header["correlation_id"])
+        else:
+            response = build_api_versions_response(
+                header["correlation_id"], error_code=35
+            )
+        self.send(conn, response)
+        return True
+
+    def handle_client(self, conn):
+        while True:
+            try:
+                data = self._read_message(conn)
+                if data is None:
+                    break
+                if not self._handle_request(data, conn):
+                    break
+            except Exception:
+                break
+        conn.close()
+
+    def serve_concurrent(self):
+        while True:
+            conn, _ = self.server.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn,))
+            thread.start()
+
 
 def main():
     print("Hello from mykafka!")
 
     kafka_instance = MyKafka("localhost", 9092)
-    conn, _ = kafka_instance.init_server()
-    data = conn.recv(1024)
-    header = parse_request_header(data)
-    if header is None:
-        return
-    if header["api_key"] == API_VERSIONS_KEY:
-        response = build_api_versions_response(header["correlation_id"])
-    else:
-        response = build_api_versions_response(header["correlation_id"], error_code=35)
-    kafka_instance.send(conn, response)
+    kafka_instance.init_server()
+    kafka_instance.serve_concurrent()
 
 
 if __name__ == "__main__":
